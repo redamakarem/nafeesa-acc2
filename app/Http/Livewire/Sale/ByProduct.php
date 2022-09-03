c8e15daa6a4050b22f552d234555816d6fb323ac
Reda Makarem <makarem.reda@gmail.com>
2022-09-03 16:28:40 +0300

    ..

--- app/Http/Livewire/Sale/ByProduct.php
+++ app/Http/Livewire/Sale/ByProduct.php
@@ -105,7 +105,7 @@ class ByProduct extends Component
         $this->query = $this->query->whereBetween('date',[$this->start_date,$this->end_date]);
         $qq = $this->query;
         $sales = $this->query->paginate($this->perPage);
-        dd($sales);
+        // dd($sales);
         return view('livewire.sale.by-product', compact('qq', 'sales'));
 
     }
