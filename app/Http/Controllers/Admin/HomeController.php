9bbc1f0bcaa9260e8ddc682f08d93703acf3594a
Reda Makarem <makarem.reda@gmail.com>
2022-09-26 02:39:24 +0300

    updated dashboard with top sales

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Sales;
 use App\Models\Finished;
 use App\Models\RawMaterial;
-use App\Models\Sales;
 use App\Models\SemiFinished;
+use Illuminate\Support\Facades\DB;
 
 class HomeController
 {
@@ -20,6 +21,27 @@ class HomeController
         $data['raw_materials'] = RawMaterial::all()->count();
         $data['semi_finished'] = SemiFinished::all()->count();
         $data['finished'] = Finished::all()->count();
+
+        $data['most_selling_quantity'] = DB::table('sales')
+        ->select('item_id','name_ar',DB::raw('count(item_id) as total_sales'))
+        ->join('finisheds', 'item_id', '=', 'finisheds.id')
+        ->groupBy('item_id')
+        ->orderBy('total_sales','desc')
+        ->get()->first();
+
+        $data['most_selling_price'] = DB::table('sales')
+        ->select('item_id','name_ar',DB::raw('sum(selling_price) as total_price'))
+        ->join('finisheds', 'item_id', '=', 'finisheds.id')
+        ->groupBy('item_id')
+        ->orderBy('total_price','desc')
+        ->get()->first();
+
+        $data['most_selling_profit'] = DB::table('sales')
+        ->select('item_id','name_ar',DB::raw('sum(profit) as total_profit'))
+        ->join('finisheds', 'item_id', '=', 'finisheds.id')
+        ->groupBy('item_id')
+        ->orderBy('total_profit','desc')
+        ->get()->first();
 
         return view('admin.home',compact('data'));
     }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.admin')
 @section('content')
-
     <div class="row">
         <div class="card bg-blueGray-100 w-full">
             <div class="card-header">
@@ -12,7 +11,9 @@
             </div>
 
             <div class="card-body">
-                <h5 class="md:block text-left md:pb-2 text-blueGray-700 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0">Sales</h5>
+                <h5
+                    class="md:block text-left md:pb-2 text-blueGray-700 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0">
+                    Sales</h5>
                 <div class="grid grid-cols-12 gap-6">
 
 
@@ -23,14 +24,12 @@
 
                             <h2 class="text-2xl font-semibold text-slate-800 mb-2">Transactions</h2>
                             <div class="flex items-start">
-                                <div class="text-3xl font-bold text-slate-800 mr-2">{{$data['transactions']}}</div>
-{{--                                <div class="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full">+49%--}}
-{{--                                </div>--}}
+                                <div class="text-3xl font-bold text-slate-800 mr-2">{{ $data['transactions'] }}</div>
                             </div>
                         </div>
                         <div class="grow">
                             <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
-                                    width="709" height="192"></canvas>
+                                width="709" height="192"></canvas>
                         </div>
                     </div>
                     <div
@@ -39,14 +38,13 @@
 
                             <h2 class="text-2xl font-semibold text-slate-800 mb-2">Total Sales</h2>
                             <div class="flex items-start">
-                                <div class="text-3xl font-bold text-slate-800 mr-2">K.D  {{number_format($data['total_sales'],3, '.', ',')}}</div>
-{{--                                <div class="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full">+49%--}}
-{{--                                </div>--}}
+                                <div class="text-3xl font-bold text-slate-800 mr-2">K.D
+                                    {{ number_format($data['total_sales'], 3, '.', ',') }}</div>
                             </div>
                         </div>
                         <div class="grow">
                             <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
-                                    width="709" height="192"></canvas>
+                                width="709" height="192"></canvas>
                         </div>
                     </div>
                     <div
@@ -55,14 +53,13 @@
 
                             <h2 class="text-2xl font-semibold text-slate-800 mb-2">Costs</h2>
                             <div class="flex items-start">
-                                <div class="text-3xl font-bold text-slate-800 mr-2">K.D  {{number_format($data['costs'],3, '.', ',')}}</div>
-{{--                                <div class="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full">+49%--}}
-{{--                                </div>--}}
+                                <div class="text-3xl font-bold text-slate-800 mr-2">K.D
+                                    {{ number_format($data['costs'], 3, '.', ',') }}</div>
                             </div>
                         </div>
                         <div class="grow">
                             <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
-                                    width="709" height="192"></canvas>
+                                width="709" height="192"></canvas>
                         </div>
                     </div>
                     <div
@@ -71,14 +68,13 @@
 
                             <h2 class="text-2xl font-semibold text-slate-800 mb-2">Profits</h2>
                             <div class="flex items-start">
-                                <div class="text-3xl font-bold text-slate-800 mr-2">K.D  {{number_format($data['profit'],3, '.', ',')}}</div>
-{{--                                <div class="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full">+49%--}}
-{{--                                </div>--}}
+                                <div class="text-3xl font-bold text-slate-800 mr-2">K.D
+                                    {{ number_format($data['profit'], 3, '.', ',') }}</div>
                             </div>
                         </div>
                         <div class="grow">
                             <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
-                                    width="709" height="192"></canvas>
+                                width="709" height="192"></canvas>
                         </div>
                     </div>
 
@@ -86,25 +82,22 @@
                 </div>
 
 
-                <h5 class="md:block text-left md:pb-2 text-blueGray-700 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0">Items</h5>
+                <h5
+                    class="md:block text-left md:pb-2 text-blueGray-700 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0">
+                    Items</h5>
                 <div class="grid grid-cols-12 gap-6">
-
-
-
                     <div
                         class="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white shadow-lg rounded-sm border border-slate-200">
                         <div class="px-5 pt-5">
 
                             <h2 class="text-2xl font-semibold text-slate-800 mb-2">Raw Materials</h2>
                             <div class="flex items-start">
-                                <div class="text-3xl font-bold text-slate-800 mr-2">{{$data['raw_materials']}} items</div>
-{{--                                <div class="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full">+49%--}}
-{{--                                </div>--}}
+                                <div class="text-3xl font-bold text-slate-800 mr-2">{{ $data['raw_materials'] }} items</div>
                             </div>
                         </div>
                         <div class="grow">
                             <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
-                                    width="709" height="192"></canvas>
+                                width="709" height="192"></canvas>
                         </div>
                     </div>
                     <div
@@ -113,14 +106,12 @@
 
                             <h2 class="text-2xl font-semibold text-slate-800 mb-2">Semi Finished</h2>
                             <div class="flex items-start">
-                                <div class="text-3xl font-bold text-slate-800 mr-2">{{$data['semi_finished']}} items</div>
-{{--                                <div class="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full">+49%--}}
-{{--                                </div>--}}
+                                <div class="text-3xl font-bold text-slate-800 mr-2">{{ $data['semi_finished'] }} items</div>
                             </div>
                         </div>
                         <div class="grow">
                             <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
-                                    width="709" height="192"></canvas>
+                                width="709" height="192"></canvas>
                         </div>
                     </div>
                     <div
@@ -129,22 +120,72 @@
 
                             <h2 class="text-2xl font-semibold text-slate-800 mb-2">Finished</h2>
                             <div class="flex items-start">
-                                <div class="text-3xl font-bold text-slate-800 mr-2">{{$data['finished']}} items</div>
-{{--                                <div class="text-sm font-semibold text-white px-1.5 bg-green-500 rounded-full">+49%--}}
-{{--                                </div>--}}
+                                <div class="text-3xl font-bold text-slate-800 mr-2">{{ $data['finished'] }} items</div>
                             </div>
                         </div>
                         <div class="grow">
                             <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
-                                    width="709" height="192"></canvas>
+                                width="709" height="192"></canvas>
                         </div>
                     </div>
 
 
 
                 </div>
+                <h5
+                    class="md:block text-left md:pb-2 text-blueGray-700 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0">
+                    Top Products</h5>
+                    <div class="grid grid-cols-12 gap-6">
+                        <div
+                            class="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white shadow-lg rounded-sm border border-slate-200">
+                            <div class="px-5 pt-5">
+    
+                                <h2 class="text-2xl font-semibold text-slate-800 mb-2">Per Quantity</h2>
+                                <div class="flex items-start">
+                                    <div class="text-3xl font-bold text-slate-800 mr-2">{{ $data['most_selling_quantity']->name_ar }} </div>
+                                </div>
+                                <div>Sales: {{ $data['most_selling_quantity']->total_sales }} </div>
+                            </div>
+                            <div class="grow">
+                                <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
+                                    width="709" height="192"></canvas>
+                            </div>
+                        </div>
+                        <div
+                            class="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white shadow-lg rounded-sm border border-slate-200">
+                            <div class="px-5 pt-5">
+    
+                                <h2 class="text-2xl font-semibold text-slate-800 mb-2">Per Price</h2>
+                                <div class="flex items-start">
+                                    <div class="text-3xl font-bold text-slate-800 mr-2">{{ $data['most_selling_price']->name_ar }}</div>
+                                </div>
+                                <div>Amount: KD. {{ $data['most_selling_price']->total_price }} </div>
+                            </div>
+                            <div class="grow">
+                                <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
+                                    width="709" height="192"></canvas>
+                            </div>
+                        </div>
+                        <div
+                            class="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white shadow-lg rounded-sm border border-slate-200">
+                            <div class="px-5 pt-5">
+    
+                                <h2 class="text-2xl font-semibold text-slate-800 mb-2">Per Profit</h2>
+                                <div class="flex items-start">
+                                    <div class="text-3xl font-bold text-slate-800 mr-2">{{ $data['most_selling_profit']->name_ar }}</div>
+                                </div>
+                                <div>Amount: KD. {{ $data['most_selling_profit']->total_profit }} </div>
+                            </div>
+                            <div class="grow">
+                                <canvas style="display: block; box-sizing: border-box; height: 128px; width: 472.667px;"
+                                    width="709" height="192"></canvas>
+                            </div>
+                        </div>
+    
+    
+    
+                    </div>
             </div>
         </div>
     </div>
-
 @endsection
