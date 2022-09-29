4e9a69be599b6f028b1db55e4edf9fa5314c1940
Reda Makarem <makarem.reda@gmail.com>
2022-09-29 12:48:43 +0300

    ..

--- resources/views/livewire/reports/top-products.blade.php
+++ resources/views/livewire/reports/top-products.blade.php
@@ -5,21 +5,42 @@
 @push('scripts')
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
-    var top_products = @json($top_products);
-    console.log(top_products);
-    var series = [];
-    var labels = [];
-    top_products.map(product =>{
-        series.push(product['total_sales']);
-        labels.push(product['name_ar']);
-    });
+    // var top_products = @json($top_products);
+    // console.log(top_products);
+    // var series = [];
+    // var labels = [];
+    // top_products.map(product =>{
+    //     series.push(product['total_sales']);
+    //     labels.push(product['name_ar']);
+    // });
+    // var options = {
+    //       series: series,
+    //       chart: {
+    //       width: 750,
+    //       type: 'donut',
+    //     },
+    //     labels: labels,
+    //     responsive: [{
+    //       breakpoint: 480,
+    //       options: {
+    //         chart: {
+    //           width: 200
+    //         },
+    //         legend: {
+    //           position: 'bottom'
+    //         }
+    //       }
+    //     }]
+    //     };
+
+    //     var chart = new ApexCharts(document.querySelector("#top-products"), options);
+    //     chart.render();
+
     var options = {
-          series: series,
+          series: [44, 55, 41, 17, 15],
           chart: {
-          width: 750,
           type: 'donut',
         },
-        labels: labels,
         responsive: [{
           breakpoint: 480,
           options: {
